0eff979d15c3b481c695a2d151118e6ee3987646
zzwrich <1559763027@qq.com>
2023-10-28 09:06:55 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<script setup>
+	import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+	<div class="grid-container">
+		<div class="grid-item block1">
+			<!-- <HelloWorld /> -->
+			<div class="sub-grid1">
+				<div class="sub-grid-item1 sub-block1">
+					属性筛选
+				</div>
+				<div class="sub-grid-item1 sub-block2">
+					属性筛选
+				</div>
+				<div class="sub-grid-item1 sub-block3">
+					属性筛选
+				</div>
+				<div class="sub-grid-item1 sub-block4">
+					属性筛选
+				</div>
+				<div class="sub-grid-item1 sub-block5">
+					属性筛选
+				</div>
+				<div class="sub-grid-item1 sub-block6">
+					属性筛选
+				</div>
+			</div>
+			<div class="sub-grid2">
+				<div class="sub-grid-item2 sub-block1">
+					时间段筛选
+				</div>
+				<div class="sub-grid-item2 sub-block2">
+					子序列筛选
+				</div>
+				<div class="sub-grid-item2 sub-block3">
+					子序列筛选
+				</div>
+				<div class="sub-grid-item2 sub-block4">
+					其他控件
+				</div>
+			</div>
+			<div class="sub-grid3">
+				筛选后可用的数据块
+			</div>
+		</div>
+		<div class="grid-item block2">
+			运算区
+		</div>
+		<div class="grid-item block3">
+			文字详细信息
+		</div>
+		<div class="grid-item block4">
+			运算结果展示区
+		</div>
+		<div class="grid-item block5">
+			运算块
+		</div>
+	</div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+  <h1>可视分析系统</h1>
+  <div>
+    {{ msg }}
+  </div>
+
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'Hello',
+  data () {
+    return {
+      msg: '初始化'
+    }
+  },
+  methods: {
+    getMessage () {
+      const path = 'http://127.0.0.1:5000/hello'
+
+      axios.get(path)
+          .then(res => {
+            this.msg = res.data['test']
+          }).catch(error => {
+        console.error(error)
+      })
+    }
+  },
+  created () {
+    // this.getMessage()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
+
